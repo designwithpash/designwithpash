90e785b0c9afcea8dbf00f30bbf9c8c7dbfc9dac
designwithpash <designwithpash@gmail.com>
2024-10-23 00:18:46 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,4 @@
+const stickyButton=document.getElementById("stickyCircle");
+stickyButton.addEventListener("click", () => {
+        window.scrollTo({top:0,left:0,behaviour:"smooth"})
+})
--- index.js
+++ index.js
@@ -64,3 +64,9 @@ video.addEventListener("ended", () => {
 video.addEventListener("seeked", () => {
     playButton.style.display='none';
 })
+
+
+const stickyButton=document.getElementById("stickyCircle");
+stickyButton.addEventListener("click", () => {
+        window.scrollTo({top:0,left:0,behaviour:"smooth"})
+})
